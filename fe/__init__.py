79a1a82a75dad2ae914235c742497bf9b6d46640
LoopThrough-i-j <dganguly1120@gmail.com>
2020-10-02 02:19:23 +0530

    updates the render route

--- fe/__init__.py
+++ fe/__init__.py
@@ -11,10 +11,13 @@ def create_app():
     # # Import all v2 blueprints
     from .blueprints.base import base
     from .blueprints.auth import authorization
+    from .blueprints.render import render
 
     # # Register blueprints
-    app.register_blueprint(base, url_prefix='/')
+    app.register_blueprint(render, url_prefix='/')
+    app.register_blueprint(base, url_prefix='/base')
     app.register_blueprint(authorization, url_prefix='/auth')
+    
 
     # from .announce import anc
     # app.register_blueprint(anc,url_prefix='/')
